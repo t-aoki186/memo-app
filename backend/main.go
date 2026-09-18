18d572defe5021f603ebd862f14f12869ef2ed95
taoki <84th.tccaoki@gmail.com>
2026-09-18 23:27:39 +0900

    feat(api): hello,worldを返せるように

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,22 @@
-package backend
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/api/hello", helloHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{
+		"message": "hello, world!",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(response)
+}
