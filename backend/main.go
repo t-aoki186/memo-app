04da4b53793bf9372706976ec90707a7d4af88be
taoki <84th.tccaoki@gmail.com>
2026-09-18 23:55:23 +0900

    chore(api): サーバー起動時にメッセージを表示するように修正

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/api/hello", helloHandler)
 
+	fmt.Println("Server is running on *:8080")
 	http.ListenAndServe(":8080", nil) // 指定なし, [net/http]のデフォルト処理を使用する。
 }
 
